da1c049f4506b818f1ba50b01b50087d9b856439
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2022-10-11 11:02:15 -0300

    [Feature] Added tests

---
+++ test/MamisSolidarias.WebAPI.Campaigns.Test/Endpoints/Campaigns.Mochi.Id.Delete.Test.cs
@@ -0,0 +1,76 @@
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using MamisSolidarias.Infrastructure.Campaigns.Models;
+using MamisSolidarias.Utils.Test;
+using MamisSolidarias.WebAPI.Campaigns.Endpoints.Campaigns.Mochi.Id.DELETE;
+using MamisSolidarias.WebAPI.Campaigns.Utils;
+using Moq;
+using NUnit.Framework;
+
+namespace MamisSolidarias.WebAPI.Campaigns.Endpoints;
+
+internal sealed class Campaigns_Mochi_Id_Delete_Test
+{
+    private Endpoint _endpoint = null!;
+    private readonly Mock<DbAccess> _mockDb = new();
+
+    [SetUp]
+    public void Setup()
+    {
+        _endpoint = EndpointFactory.CreateEndpoint<Endpoint>(null, _mockDb.Object);
+    }
+
+    [TearDown]
+    public void Teardown()
+    {
+        _mockDb.Reset();
+    }
+
+    [Test]
+    public async Task CampaignExists_Success()
+    {
+        // Arrange
+        Mochi mochi = DataFactory.GetMochi();
+        _mockDb.Setup(x => x.GetMochiAsync(
+            It.Is<int>(t=> t == mochi.Id),
+            It.IsAny<CancellationToken>()
+            )
+        ).ReturnsAsync(mochi);
+
+        _mockDb.Setup(t => t.DeleteMochiAsync(
+                It.Is<Mochi>(r => r.Id == mochi.Id),
+                It.IsAny<CancellationToken>()
+            )
+        ).Returns(Task.CompletedTask);
+
+        var req = new Request {Id = mochi.Id};
+        
+        // Act
+        await _endpoint.HandleAsync(req, CancellationToken.None);
+        
+        // Assert
+        _endpoint.HttpContext.Response.StatusCode.Should().Be(200);
+    }
+
+    [Test]
+    public async Task CampaignDoesNotExists_Fails()
+    {
+        // Arrange
+        Mochi mochi = DataFactory.GetMochi();
+        _mockDb.Setup(x => x.GetMochiAsync(
+            It.Is<int>(t=> t == mochi.Id),
+            It.IsAny<CancellationToken>()
+            )
+        ).ReturnsAsync((Mochi?)null);
+
+
+        var req = new Request {Id = mochi.Id};
+        
+        // Act
+        await _endpoint.HandleAsync(req, CancellationToken.None);
+        
+        // Assert
+        _endpoint.HttpContext.Response.StatusCode.Should().Be(404);
+    }
+}
