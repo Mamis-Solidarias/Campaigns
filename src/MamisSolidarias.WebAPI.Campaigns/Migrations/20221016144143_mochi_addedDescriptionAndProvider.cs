caf2d35b6d57f9eee6c11ab4fd2783caf15fb63c
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2022-10-16 12:16:41 -0300

    [Feature] Updated db models

---
+++ src/MamisSolidarias.WebAPI.Campaigns/Migrations/20221016144143_mochi_addedDescriptionAndProvider.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MamisSolidarias.WebAPI.Campaigns.Migrations
+{
+    public partial class mochi_addedDescriptionAndProvider : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "MochiCampaigns",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Provider",
+                table: "MochiCampaigns",
+                type: "character varying(300)",
+                maxLength: 300,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "MochiCampaigns");
+
+            migrationBuilder.DropColumn(
+                name: "Provider",
+                table: "MochiCampaigns");
+        }
+    }
+}
