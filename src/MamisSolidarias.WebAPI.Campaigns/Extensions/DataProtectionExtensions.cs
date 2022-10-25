9f35e14fb924c6e28f2dd66e2401fdce893de043
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2022-10-24 23:31:01 -0300

    [Fix] Persist secure keys over container restarts (#53)

---
+++ src/MamisSolidarias.WebAPI.Campaigns/Extensions/DataProtectionExtensions.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption;
+using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption.ConfigurationModel;
+
+namespace MamisSolidarias.WebAPI.Campaigns.Extensions;
+
+internal static class DataProtectionExtensions
+{
+    public static void AddDataProtection(this IServiceCollection services, IConfiguration configuration)
+    {
+        var dataProtectionKeysPath = configuration.GetValue<string>("DataProtectionKeysPath");
+        if (!string.IsNullOrWhiteSpace(dataProtectionKeysPath))
+        {
+            services.AddDataProtection().PersistKeysToFileSystem(new DirectoryInfo(dataProtectionKeysPath))
+                .UseCryptographicAlgorithms(new AuthenticatedEncryptorConfiguration
+                {
+                    EncryptionAlgorithm = EncryptionAlgorithm.AES_256_CBC,
+                    ValidationAlgorithm = ValidationAlgorithm.HMACSHA256
+                });
+        }
+    }
+}
