64d13a00517323665dee084acee607b8a6b91483
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2023-01-15 09:57:56 -0300

    [Feature] Implemented endpoint

---
+++ src/MamisSolidarias.WebAPI.Campaigns/Endpoints/Campaigns/Abrigaditos/Id/PUT/Description.cs
@@ -0,0 +1,25 @@
+using FastEndpoints;
+
+namespace MamisSolidarias.WebAPI.Campaigns.Endpoints.Campaigns.Abrigaditos.Id.PUT;
+
+internal sealed class Description : Summary<Endpoint>
+{
+    public Description()
+    {
+        Summary = "It edits an edition of 'Abrigaditos'";
+        ExampleRequest = new Request
+        {
+            AddedBeneficiaries = new[] { 1, 2, 3 },
+            RemovedBeneficiaries = new[] { 4, 5, 6 },
+            Description = "Description of the edition",
+            Provider = "Provider of the edition",
+            FundraiserGoal = 9999999,
+            Id = 123
+        };
+        Response();
+        Response(400, "Invalid request");
+        Response(401, "Unauthorized");
+        Response(403, "Forbidden");
+        Response(404, "Campaign not found");
+    }
+}
