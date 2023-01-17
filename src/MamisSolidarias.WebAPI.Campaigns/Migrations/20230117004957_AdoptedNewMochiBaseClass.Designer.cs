02c232b64a73e68e958d95dbbf52ef7a8e681dd8
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2023-01-16 21:51:27 -0300

    [Chore] Created new migration

--- src/MamisSolidarias.WebAPI.Campaigns/Migrations/20230117004957_AdoptedNewMochiBaseClass.Designer.cs
+++ src/MamisSolidarias.WebAPI.Campaigns/Migrations/20230117004957_AdoptedNewMochiBaseClass.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using MamisSolidarias.Infrastructure.Campaigns;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MamisSolidarias.WebAPI.Campaigns.Migrations
 {
     [DbContext(typeof(CampaignsDbContext))]
-    partial class CampaignsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230117004957_AdoptedNewMochiBaseClass")]
+    partial class AdoptedNewMochiBaseClass
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
