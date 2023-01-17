24e68e89824a9e28532b52954b90eb8ad3e5c04b
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2023-01-17 12:18:12 -0300

    [Refactor] Removed DbAccess from delete /campaigns/juntos/id

--- test/MamisSolidarias.WebAPI.Campaigns.Test/Endpoints/Campaigns.Juntos.Id.Delete.Test.cs
+++ test/MamisSolidarias.WebAPI.Campaigns.Test/Endpoints/Campaigns.Juntos.Id.Delete.Test.cs
@@ -2,49 +2,23 @@ using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
 using MamisSolidarias.Infrastructure.Campaigns.Models.JuntosALaPar;
-using MamisSolidarias.Utils.Test;
 using MamisSolidarias.WebAPI.Campaigns.Endpoints.Campaigns.Juntos.Id.DELETE;
 using MamisSolidarias.WebAPI.Campaigns.Utils;
 using Microsoft.EntityFrameworkCore;
-using Moq;
 using NUnit.Framework;
 
 namespace MamisSolidarias.WebAPI.Campaigns.Endpoints;
 
-public class CampaignsJuntosIdDeleteTest
+internal class CampaignsJuntosIdDeleteTest : EndpointTest<Endpoint>
 {
-    private readonly Mock<DbAccess> _mockDb = new();
-    private Endpoint _endpoint = null!;
-
-    [SetUp]
-    public void Setup()
-    {
-        _endpoint = EndpointFactory.CreateEndpoint<Endpoint>(null, _mockDb.Object);
-    }
-
-    [TearDown]
-    public void Teardown()
-    {
-        _mockDb.Reset();
-    }
+    protected override object?[] ConstructorArguments =>
+        new object?[] { _dbContext };
 
     [Test]
     public async Task CampaignExists_Success()
     {
         // Arrange
-        JuntosCampaign campaign = DataFactory.GetJuntosCampaign();
-        _mockDb.Setup(x => x.CampaignExists(
-                It.Is<int>(t => t == campaign.Id),
-                It.IsAny<CancellationToken>()
-            )
-        ).ReturnsAsync(true);
-
-        _mockDb.Setup(t => t.DeleteCampaign(
-                It.Is<int>(r => r == campaign.Id),
-                It.IsAny<CancellationToken>()
-            )
-        ).Returns(Task.CompletedTask);
-
+        var campaign = _dataFactory.GenerateJuntosCampaign().Build();
         var req = new Request { Id = campaign.Id };
 
         // Act
@@ -52,6 +26,8 @@ public class CampaignsJuntosIdDeleteTest
 
         // Assert
         _endpoint.HttpContext.Response.StatusCode.Should().Be(200);
+        var dbCampaign = await _dbContext.JuntosCampaigns.FirstOrDefaultAsync(x => x.Id == campaign.Id);
+        dbCampaign.Should().BeNull();
     }
 
     [Test]
@@ -59,13 +35,6 @@ public class CampaignsJuntosIdDeleteTest
     {
         // Arrange
         JuntosCampaign campaign = DataFactory.GetJuntosCampaign();
-        _mockDb.Setup(x => x.CampaignExists(
-                It.Is<int>(t => t == campaign.Id),
-                It.IsAny<CancellationToken>()
-            )
-        ).ReturnsAsync(false);
-
-
         var req = new Request { Id = campaign.Id };
 
         // Act
@@ -73,31 +42,5 @@ public class CampaignsJuntosIdDeleteTest
 
         // Assert
         _endpoint.HttpContext.Response.StatusCode.Should().Be(404);
-    }
-
-    [Test]
-    public async Task UnknownDatabaseError_Fails()
-    {
-        // Arrange
-        JuntosCampaign campaign = DataFactory.GetJuntosCampaign();
-        _mockDb.Setup(x => x.CampaignExists(
-                It.Is<int>(t => t == campaign.Id),
-                It.IsAny<CancellationToken>()
-            )
-        ).ReturnsAsync(true);
-
-        _mockDb.Setup(t => t.DeleteCampaign(
-                It.Is<int>(r => r == campaign.Id),
-                It.IsAny<CancellationToken>()
-            )
-        ).ThrowsAsync(new DbUpdateException());
-
-        var req = new Request { Id = campaign.Id };
-
-        // Act
-        await _endpoint.HandleAsync(req, CancellationToken.None);
-
-        // Assert
-        _endpoint.HttpContext.Response.StatusCode.Should().Be(500);
     }
 }
