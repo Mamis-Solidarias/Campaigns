d1639b573dbfa341e2eb5cb89263a28dd200a19f
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2023-02-19 10:07:32 -0300

    [Feature] Added GetAbrigaditosCampaign by Community and Edition (#305)

--- src/MamisSolidarias.WebAPI.Campaigns/Queries/AbrigaditosQueries.cs
+++ src/MamisSolidarias.WebAPI.Campaigns/Queries/AbrigaditosQueries.cs
@@ -2,16 +2,29 @@ using HotChocolate.AspNetCore.Authorization;
 using HotChocolate.Types;
 using MamisSolidarias.Infrastructure.Campaigns;
 using MamisSolidarias.Infrastructure.Campaigns.Models.Abrigaditos;
+using Microsoft.EntityFrameworkCore;
 
 namespace MamisSolidarias.WebAPI.Campaigns.Queries;
 
 [ExtendObjectType("Query")]
 public class AbrigaditosQueries
 {
+    
     [Authorize(Policy = "CanRead")]
     [UseFirstOrDefault]
     [UseProjection]
-    public IQueryable<AbrigaditosCampaign> GetAbrigaditosCampaign(CampaignsDbContext db, int id)
+    public IQueryable<AbrigaditosCampaign> GetAbrigaditosCampaign(CampaignsDbContext context, string community, string edition)
+    {
+        return context.AbrigaditosCampaigns
+            .AsNoTracking()
+            .Where(t => t.CommunityId == community && t.Edition == edition);
+    }
+
+    
+    [Authorize(Policy = "CanRead")]
+    [UseFirstOrDefault]
+    [UseProjection]
+    public IQueryable<AbrigaditosCampaign> GetAbrigaditosCampaignById(CampaignsDbContext db, int id)
     {
         return db.AbrigaditosCampaigns.Where(c => c.Id == id);
     }
